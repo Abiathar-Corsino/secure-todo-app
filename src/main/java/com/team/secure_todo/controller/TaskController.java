1bf7a1bf98e0c24abe318e1f7ba5ee39d9f08cc6
marquezgerald <marquezgeraldjohn@gmail.com>
2025-06-06 16:18:19 +0800

    update

--- src/main/java/com/team/secure_todo/controller/TaskController.java
+++ src/main/java/com/team/secure_todo/controller/TaskController.java
@@ -1,4 +1,42 @@
 package com.team.secure_todo.controller;
 
+import com.team.secure_todo.model.Task;
+import com.team.secure_todo.model.User;
+import com.team.secure_todo.repository.TaskRepository;
+import com.team.secure_todo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<Task> getTasks(Authentication auth) {
+        logger.info("User {} looking at tasks", auth.getName());
+        User user = userRepository.findByEmail(auth.getName()).orElseThrow();
+        if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            return taskRepository.findAll();
+        }
+        return taskRepository.findByUser(user);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task, Authentication auth) {
+        logger.info("User {} making task: {}", auth.getName(), task.getTitle());
+        User user = userRepository.findByEmail(auth.getName()).orElseThrow();
+        task.setUser(user);
+        return taskRepository.save(task);
+    }
 }
--- src/main/java/com/team/secure_todo/model/Task.java
+++ src/main/java/com/team/secure_todo/model/Task.java
@@ -1,4 +1,27 @@
 package com.team.secure_todo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String description;
+    @ManyToOne
+    private User user;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
 }
+
