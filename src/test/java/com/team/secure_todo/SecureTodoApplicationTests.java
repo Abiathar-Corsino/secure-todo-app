c39f9cd067819853806fe8134c97541638972025
Abiathar Corsino <abiatharcorsino@gmail.com>
2025-06-06 12:37:54 +0800

    Initial commit: Spring Boot setup by Team Corsino, Marquez, Pedroso, Rotamula, Altarehos

---
+++ src/test/java/com/team/secure_todo/SecureTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.secure_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
